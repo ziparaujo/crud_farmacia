5a50c96e9c7ef5077b010c28bd20b91294c20a1e
unknown <ziparaujo@gmail.com>
2024-07-22 12:54:46 -0300

    :sparkles: feat: crud de produto

--- src/main/java/com/generation/crudfarmacia/model/Categoria.java
+++ src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -1,14 +1,21 @@
 package com.generation.crudfarmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table
+@Table(name = "tb_categorias")
 public class Categoria {
 	
 	@Id
@@ -20,6 +27,10 @@ public class Categoria {
 	
 	@NotNull(message = "O atributo descrição é obrigatório.")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -43,6 +54,14 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 }
---
+++ src/main/java/com/generation/crudfarmacia/model/Produto.java
@@ -0,0 +1,104 @@
+package com.generation.crudfarmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String nome;
+	
+	@NotNull(message = "O atributo quantidade é obrigatorio")
+	private Integer quantidade;
+	
+	@Digits(integer= 8, fraction= 2)
+	@NotNull(message = "O atributo preço é obrigatorio")
+	private BigDecimal preco;
+
+	@NotBlank(message = "O atributo imagem é obrigatorio")
+	private String image;
+	
+	@NotBlank(message = "O atributo descrição é obrigatorio")
+	@Size(max = 500, message = "O atributo deve conter no maximo 500 caracteres")
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
