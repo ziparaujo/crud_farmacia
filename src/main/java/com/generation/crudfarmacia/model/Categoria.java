b8f6065c302f851220d7a485c9c744ebd4d7488d
unknown <ziparaujo@gmail.com>
2024-07-22 12:36:29 -0300

    :sparkles: feat: crud da categoria

---
+++ src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -0,0 +1,48 @@
+package com.generation.crudfarmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo nome é obrigatório.")
+	private String nome;
+	
+	@NotNull(message = "O atributo descrição é obrigatório.")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
